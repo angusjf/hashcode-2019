a2ca43768d383c7667b29aa77457127f5e8cc382
Angus Macbook <el17ajf@leeds.ac.uk>
2019-03-10 10:06:23 +0000

    refactor to use zip

--- Main.hs
+++ Main.hs
@@ -11,12 +11,13 @@ data Image = Image
 data Slide = HSlide Image | VSlide (Image, Image) deriving (Eq)
 
 main :: IO ()
-main = interact $ unlines . map show . arrange . toSlides . textFileToImages
-
-textFileToImages :: String -> [Image]
-textFileToImages s = setIndexes 0 $ map toImage $ tail . lines $ s
+main = interact $ slidesToText . arrange . textToSlides
   where
-    inputData = (tail . lines) s
+    textToSlides = toSlides . toImages . tail . lines
+    slidesToText = unlines . map show
+
+toImages :: [String] -> [Image]
+toImages = zipWith toImage [0..]
 
 toSlides :: [Image] -> [Slide]
 toSlides images = (map HSlide his) ++ (map VSlide vis)
@@ -25,33 +26,24 @@ toSlides images = (map HSlide his) ++ (map VSlide vis)
     vis = pairUp $ filter (not . horizontal) images
 
 arrange :: [Slide] -> [Slide]
-arrange (x:xs)
-    | length xs == 0 = [x]
-    | otherwise = x : arrange rest
+arrange [x] = [x]
+arrange (x:xs) = x : arrange rest
   where
     rest = match : (without match xs)
     match = bestMatch x xs
 
------
-toImage :: String -> Image
-toImage s = Image index horizontal tags
-  where
-    index = 0
-    ws = words s
-    horizontal = head ws == "H"
-    tags = tail (tail ws)
-
-setIndexes :: Int -> [Image] -> [Image]
-setIndexes _ [] = []
-setIndexes n ((Image i h t):xs) = (Image n h t) : setIndexes (n + 1) xs
-
----
+toImage :: Int -> String -> Image
+toImage index s = Image index (orientation == "H") tags
+  where (orientation:_:tags) = words s
 
 without :: (Eq a) => a -> [a] -> [a]
 without n xs = filter (\i -> i /= n) xs
 
 bestMatch :: Slide -> [Slide] -> Slide
-bestMatch x xs = maximumBy (\a b -> compare (getScore x a) (getScore x b)) xs
+bestMatch candidate list = maximumBy (compareSlides candidate) list
+
+compareSlides :: Slide -> Slide -> Slide -> Ordering
+compareSlides s a b = compare (getScore s a) (getScore s b)
 
 getScore :: Slide -> Slide -> Int
 getScore a b = min3 (length atags) (length btags) (length bothtags)
@@ -69,7 +61,7 @@ instance Show Slide where
     show (HSlide (Image i _ _)) = show i
     show (VSlide ((Image ai _ _), (Image bi _ _))) = show ai ++ " " ++ show bi
 
-pairUp :: [x] -> [(x, x)]
+pairUp :: [a] -> [(a, a)]
 pairUp [] = []
 pairUp (x:y:xs) = (x, y) : pairUp(xs)
 pairUp (x:[]) = error "odd input array length"
--- README.md
+++ README.md
@@ -3,4 +3,4 @@ my submission for 2019 google's hashcode competition
 
 I GOT 2nd PLACE IN LEEDS UNI HUB!
 
-I later rewrote it in haskell, just for fun
+Originally done in java, but I later rewrote it in haskell, just for fun
